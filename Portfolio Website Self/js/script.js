8ae089e6daeafd4cc10cb4091b3050b7d1c58855
Jyash0547 <jyash0547@gmail.com>
2026-08-14 21:18:34 +0530

    Add Portfolio Website

---
+++ Portfolio Website Self/js/script.js
@@ -0,0 +1,11 @@
+
+let typed = new Typed("#input",{
+    strings:["Frontend Developer","Javascript Programmer","Web Designer","Database Handler"],
+    typeSpeed:100,
+    backSpeed:100,
+    startDelay:100,
+    backDelay:100,
+    loop:true,
+    showCursor:true,
+    cursorChar:"|",
+})
